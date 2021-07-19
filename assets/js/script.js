5b142488984c0a27d1ef4e94cb84d86557395730
saumilkt <saumilkt@gmail.com>
2021-07-18 17:31:27 -0700

    declaring global vars

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var quizStatus = true; // Know the status of the quiz. Quiz is not running = false , running = true
+var questionNumber = 0; // Track the question answered.
+var answerNumber = 0; // Track next answers to show
+var score = 0; // Max value by decreasing each wrong answer
+var highScore = 50; // Score add fix for ticking timer.
+var finalAnswerCheck = 0 // If last answer was wrong it will be validated outside of the time interval and then display as enabled = 1 
+var checkTimes = 1 // Check timer times call for function on last question
+var viewHighScoresBtnEl = document.getElementById('view-high-scores'); // View High Scores Btn El
+var startQuizBtnEl = document.getElementById('start-quiz'); // Start Quiz button Btn El
+var answer1BtnEl = document.getElementById('answer1'); // Start Quiz button Btn El
+var answer2BtnEl = document.getElementById('answer2'); // Start Quiz button Btn El
+var answer3BtnEl = document.getElementById('answer3'); // Start Quiz button Btn El
+var answer4BtnEl = document.getElementById('answer4'); // Start Quiz button Btn El
+var submitScoreEl = document.getElementById('submitScore'); // Start Quiz button Btn El
+var questionsEl = document.getElementById('questions'); // Questions for the main Div
+var mainDivEl = document.getElementById('mainDiv'); // Main div container for all elements except for header elements
+var htmlTimeLeft = document.getElementById('timeLeft'); // Display counter @ the html level.
+var answerCorrectWrong = document.getElementById('answerCorrectWrong'); // Display counter @ the html level.
+var questionDisplayEl = document.createElement("questionDisplay"); // Display Question
+var finalScoreDisplayEl = document.createElement("finalScoreDisplay"); // Display Question
+var enterInitialsEl = document.createElement("enterInitials"); // Enter initials
+var enterInitialsTextAreaEl = document.createElement("enterInitialsTextArea"); // TextArea
+var button1234 = document.createElement("button"); // Test answer 1
+var timeLeft = 60; // Global time left assignment counter
+
